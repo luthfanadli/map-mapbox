d546dd667678af2ae3597dd83123f6fd3bd55a9f
luthfanadli <luthfan30@gmail.com>
2023-01-24 17:18:26 +0700

    feat: set env, cleaning code

--- map-app/src/components/Map.js
+++ map-app/src/components/Map.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import "../style/Map.css";
 import mapboxgl from 'mapbox-gl';
 import { useSelector } from 'react-redux';
 
 function Map({ lnglat }) {
-    const featureCollection = useSelector((state) => state.featureCollection)
-    const features = featureCollection[0].features
-    const darkMode = useSelector((state) => state.darkMode)
+    const darkMode = useSelector((state) => state.darkMode);
     const lightMap = "mapbox://styles/mapbox/light-v11";
-    const darkMap = "mapbox://styles/mapbox/dark-v11"
+    const darkMap = "mapbox://styles/mapbox/dark-v11";
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(114.594376);
-    const [lat, setLat] = useState(-3.318607);
-    const [zoom, setZoom] = useState(4);
-    const marker = new mapboxgl.Marker()
+    const lng = 114.594376;
+    const lat = -3.318607;
+    const zoom = 4;
+    const marker = new mapboxgl.Marker();
 
-    mapboxgl.accessToken = "pk.eyJ1IjoibHV0aGZhbndpY2Frc29ubyIsImEiOiJjbGJnY2gzMGEwZXBwM29xd3dmbzRtdHRkIn0.ty0WZVkVNGXyyxOzNiW7QA"
+    mapboxgl.accessToken = `${process.env.REACT_APP_ACCESS_TOKEN}`
 
     useEffect(() => {
         if (map.current) return;
@@ -45,20 +43,6 @@ function Map({ lnglat }) {
                 .addTo(map.current)
         }
     }, [lnglat])
-    
-    
-    // useEffect(() => {
-    //     if (features) {
-    //         features.map((poi) => {
-    //             console.log(poi.geometry.coordinates[0], poi.geometry.coordinates[1], 'KKKKKKKKKKKK');
-    //             marker
-    //                 .setLngLat([poi.geometry.coordinates[0], poi.geometry.coordinates[1]])
-    //                 .addTo(map.current)
-                    
-    //         })
-            
-    //     }
-    // }, [features])
 
     return (
         <div className="map-container">
